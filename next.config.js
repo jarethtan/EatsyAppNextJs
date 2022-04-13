e1962e0c9eef5ec2b5b57c86d866e07250f9771d
jareth tan <jareth.tan@outlook.com>
2022-04-13 21:33:46 +0800

    next config

--- next.config.js
+++ next.config.js
@@ -11,11 +11,11 @@ const generateCsp = () => {
   hash.update(nanoid());
   const production = process.env.NODE_ENV === "production";
 
-  return `default-src 'self'; style-src https://fonts.googleapis.com 'self' 'unsafe-inline'; script-src 'sha256-${hash.digest(
+  return `default-src 'self' http://localhost:3000 http://localhost:3000/api/cartStorage; style-src https://fonts.googleapis.com 'self' 'unsafe-inline'; script-src 'sha256-${hash.digest(
     "base64"
   )}' 'self' 'unsafe-inline' ${
     production ? "" : "'unsafe-eval'"
-  }; font-src https://fonts.gstatic.com 'self' data:; img-src https://res.cloudinary.com https://s.gravatar.com https://avatars.githubusercontent.com https://lh3.googleusercontent.com 'self' data:;`;
+  }; font-src https://fonts.gstatic.com 'self' data:; img-src https://res.cloudinary.com https://s.gravatar.com https://avatars.githubusercontent.com https://lh3.googleusercontent.com 'self' data:; connect-src http://localhost:3000 http://localhost:3000/api/cartStorage`;
 };
 
 module.exports = {
