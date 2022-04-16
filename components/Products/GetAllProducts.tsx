1a6cee1d5ee4a449f986306d9770e2ee12b08307
jareth tan <jareth.tan@outlook.com>
2022-04-16 15:30:01 +0800

    remove arrow up

--- components/Products/GetAllProducts.tsx
+++ components/Products/GetAllProducts.tsx
@@ -180,6 +180,57 @@ const GetAllProducts: React.FC<{ allProducts: ProductModel[] }> = (props) => {
   const Products: ProductModel[] = searchResult !== null ? searchResult : props.allProducts;
 
   return (
+    // <div>
+    //   {searchResult !== null && searchResult.length === 0 ? (
+    //     <h3 className={classes.searchMessage}>We are sorry. There are no matching results generated for your search request.</h3>
+    //   ) : (
+    //     ""
+    //   )}
+    //   {/* <button id="btnScrollUp">
+    //     <ArrowUpwardIcon fontSize="large" />
+    //   </button> */}
+    //   <Grid container justifyContent="center">
+    //     {Products.sort((a, b) => a.productName.localeCompare(b.productName)).map((product) => (
+    //       <Grid key={product._id} item xs={4} sm={3} md={4} lg={3}>
+    //         <Card className={classes.card}>
+    //           <CardActionArea disableRipple={true} component="div" sx={{ cursor: "auto" }}>
+    //             <CardMedia component="img" image={product.productImage[0]} alt={product.productName} className={classes.productImage} />
+    //             <CardContent className={classes.cardContent}>
+    //               <Typography noWrap variant="h5" component="div" className={classes.productName}>
+    //                 {product.productName}
+    //               </Typography>
+    //               <div className={classes.countryIconButton}>
+    //                 <img src={icons.find((element) => element.includes(product.productCategory.replace(/ /g, "")))} alt="" className={classes.countryImage} />
+    //               </div>
+    //               <Typography noWrap variant="body2" color="text.secondary" className={classes.description}>
+    //                 {product.productDescription}
+    //                 <br />
+    //                 <br />
+    //                 <br />
+    //               </Typography>
+    //               <Typography variant="body2" color="text.secondary" className={classes.price}>
+    //                 ${product.productPrice}
+    //               </Typography>
+    //               <Button
+    //                 onClick={(event) => {
+    //                   event.stopPropagation();
+    //                   event.preventDefault();
+    //                   dispatch(addToCart(product));
+    //                 }}
+    //                 className={classes.cartButton}
+    //               >
+    //                 Add to Cart
+    //               </Button>
+    //               <Button href={"/products/" + product._id} className={classes.detailsButton}>
+    //                 Details
+    //               </Button>
+    //             </CardContent>
+    //           </CardActionArea>
+    //         </Card>
+    //       </Grid>
+    //     ))}
+    //   </Grid>
+    // </div>
     <div>
       {searchResult !== null && searchResult.length === 0 ? (
         <h3 className={classes.searchMessage}>We are sorry. There are no matching results generated for your search request.</h3>
@@ -192,7 +243,7 @@ const GetAllProducts: React.FC<{ allProducts: ProductModel[] }> = (props) => {
       <Grid container justifyContent="center">
         {Products.sort((a, b) => a.productName.localeCompare(b.productName)).map((product) => (
           <Grid key={product._id} item xs={4} sm={3} md={4} lg={3}>
-            <Card className={classes.card}>
+            <div className={classes.card}>
               <CardActionArea disableRipple={true} component="div" sx={{ cursor: "auto" }}>
                 <CardMedia component="img" image={product.productImage[0]} alt={product.productName} className={classes.productImage} />
                 <CardContent className={classes.cardContent}>
@@ -226,7 +277,7 @@ const GetAllProducts: React.FC<{ allProducts: ProductModel[] }> = (props) => {
                   </Button>
                 </CardContent>
               </CardActionArea>
-            </Card>
+            </div>
           </Grid>
         ))}
       </Grid>
