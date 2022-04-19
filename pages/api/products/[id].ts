e51e85efb362d00edc8fe0440fcc0a19d03105a5
jareth tan <jareth.tan@outlook.com>
2022-04-19 16:13:53 +0800

    remove createproduct from api url part 34

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -18,6 +18,7 @@ const handlers = async (req: NextApiRequest, res: NextApiResponse) => {
           return getOneProduct(id as string);
         }
       case "POST":
+        console.log("CREATE PRODUCT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         return createProduct(req, res);
       case "PUT":
         return editProduct(req, res);
